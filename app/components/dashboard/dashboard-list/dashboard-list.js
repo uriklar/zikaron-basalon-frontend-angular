88303511b57c3e820a3c2d3a98ad9c4c675dc0da
Uri Klar <uri.k@phytech.com>
2016-02-05 20:34:20 +0200

    Added some more placeholders to dashboard

--- app/components/dashboard/dashboard-list/dashboard-list.js
+++ app/components/dashboard/dashboard-list/dashboard-list.js
@@ -14,6 +14,7 @@ export function dashboardList() {
           { val: 'has_survivor', name: 'צוות לאיש עדות' },
           { val: 'public', name: 'ערב פתוח' }
         ];
+
         this.data = [
           {
             name: 'אורי קלר',
